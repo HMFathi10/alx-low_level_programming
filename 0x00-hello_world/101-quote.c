e96edc85f8de206e94896a93909be7c340f2df49
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-10 13:50:35 -0700

    Added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, sentence, 59);
+char sen[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, sen, 59);
 return (1);
 }
