add164b0a2de0366dcc63331ccee118f9d417160
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-10 21:13:22 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
+/* Printing sentence using puts function */
 int main(void)
 {
-/* Printing sentence using puts function */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
