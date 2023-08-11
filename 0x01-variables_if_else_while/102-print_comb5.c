3824e668714a0b02abbcf96f6e831dda6758411d
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-11 19:20:24 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,28 @@ int main(void)
 {
 int n;
 int i;
+int j;
+int k;
 
 for (n = 0; n < 10; n++)
 {
 for (i = 0; i < 10; i++)
 {
+for (j = 0; j < 10; j++)
+{
+for (k = i + 1; k < 10; k++)
+{
 putchar(n + '0');
 putchar(i + '0');
-if (n != 9 || i != 9)
+putchar(j + '0');
+putchar(k + '0');
+
+if (n != 9 || i != 8 || j != 9 || k != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 }
