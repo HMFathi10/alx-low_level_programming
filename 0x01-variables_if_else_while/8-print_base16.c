eb64bc9651571207174ce5a6dc4a2a5a4a67a17f
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-11 18:29:53 +0200

    Added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all the numbers of base 16 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for(i = 0; i < 55; i++)
+{
+putchar(i + '0');
+if (i == 9)
+{
+i = 48;
+}
+putchar('\n');
+return (0);
+}
