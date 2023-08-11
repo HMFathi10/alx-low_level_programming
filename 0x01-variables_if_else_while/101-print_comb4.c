909a833e6e170237110bc920bc0ccf2e28ad8102
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-11 18:08:49 +0200

    Added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all possible combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int i;
+int j;
+
+for (n = 0; n < 10; n++)
+{
+for (i = n + 1; i < 10; i++)
+{
+for (j = i + 1; j < 10; J++)
+{
+putchar(n + '0');
+putchar(i + '0');
+putchar(j + '0');
+if (n != 8 || i != 9 || j != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all possible combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int i;
+int j;
+
+for (n = 0; n < 10; n++)
+{
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; J++)
+{
+putchar(n + '0');
+putchar(i + '0');
+putchar(j + '0');
+if (n != 9 || i != 9 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
