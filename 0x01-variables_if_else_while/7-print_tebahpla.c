088c4f5d559004b8896b1a7264ca8b7feb26d6ba
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-11 18:46:50 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,13 +2,15 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the alphabet in lowercaseu reverse, followed by a new line
+ * Description: This program prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        for (char c = 'z'; c >= 'a'; c--)
+        char c;
+
+        for (c = 'z'; c >= 'a'; c--)
                 putchar(c);
         putchar('\n');
         return (0);
