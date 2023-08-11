19103bf793249dc9700161fd311a596c75d4851a
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-11 18:55:39 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,25 @@
  */
 int main(void)
 {
-        int n;
-        int i;
+int n;
+int i;
 
-        for (n = 0; n < 10; n++)
-        {
-                for (i = n+1; i < 10; i++)
-                {
-                        putchar(n + '0');
-                        putchar(i + '0');
-                        if (n != 8 || i != 9)
-                        {
-                                putchar(',');
-                                putchar(' ');       
-                        }
+for (n = 0; n < 10; n++)
+{
+for (i = n+1; i < 10; i++)
+{
+putchar(n + '0');
+putchar(i + '0');
+if (n != 8 || i != 9)
+{
+putchar(',');
+putchar(' ');       
+}
                        
-                }
+}
         
-        }
-        putchar('\n');
-        return (0);
+}
+putchar('\n');
+return (0);
 }
 
