583e41d9cb7bdc5da927ab24cb6f664338b86b27
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-15 19:32:13 +0200

    Added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Computes and prints the sum
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long long int n = 0;
+	int i = 1;
+
+	while (i < 1024)
+	{
+		n += (i * 5);
+		i++;
+	}
+	i = 1;
+	while (i < 1024)
+	{
+		if (((i *3) % 5) != 0)
+			n + = (i * 3);
+		i++;
+	}
+	printf("%d", n);
+	return (0);
+}
