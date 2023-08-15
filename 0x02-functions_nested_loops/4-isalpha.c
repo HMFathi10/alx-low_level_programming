cbd3e6a130cb4b52f08f878b5a7c8d8fcf7885d9
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-15 17:13:21 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * [O
- * _islower - checks for lowercase character.
+ * _isalpha - checks for alphabet of character.
  * @c: is ASCII for char
- * Return: 0 if upper and 1 if lower
+ * Return: 0 if alpha and 1 if not alpha
  */
 int _isalpha(int c)
 {
