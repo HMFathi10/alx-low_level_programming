0f5cddf36281ecaad49c878e95dc17d955cb33de
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-15 15:16:28 +0200

    Added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints _putchar.
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[8] = "_putchar";
+	int n;
+
+	for (n = 0; n < 8; n++)
+		_putchar(str[n]);
+	_putchar('\n');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void.
+ */
+int _islower(int c)
+{
+	c >= 97 ? return (1) : return (0);
+}
