c2ec72f2d68272bb065a953e06d2b0887c2f1033
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-15 20:02:09 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,11 +7,11 @@
  */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-		return ;
 	int i;
 	int j;
 
+	if (n > 15 || n < 0)
+		return ;
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
