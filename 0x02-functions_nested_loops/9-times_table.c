1551ddd26b44eda5d69a145f9e54bec9b57ad57d
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-15 18:35:45 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,15 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			if (i * j > 9)
+			{
 				_putchar((i * j) / 10 + '0');
-			_putchar((i * j) % 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((i * j) % 10 + '0');	
+			}
 			if (j != 9)
 			{
 				_putchar(',');
