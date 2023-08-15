8e3dd2b86d93e9d71bd35498f78cb3886ab041e8
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-15 18:40:11 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,8 +21,9 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar(' ');
-				_putchar((i * j) % 10 + '0');	
+				if (j != 0)
+					_putchar(' ');
+				_putchar((i * j) % 10 + '0');
 			}
 			if (j != 9)
 			{
