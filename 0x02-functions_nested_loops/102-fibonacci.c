83dcf576035af3fd39f10483a64a0f78e6cefb9b
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-15 20:54:38 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,13 @@ int main(void)
 	long int m = 2;
 	int max;
 
-	for (max = 0; max <= 48; max++)
+	printf("%ld, %ld, ", n, m);
+	for (max = 0; max < 48; max++)
 	{
 		printf("%ld", m + n);
 		m += n;
 		n = m - n;
-		if (max != 48)
+		if (max != 47)
 			printf(", ");
 	}
 	return (0);
