38ed0f6b2cf9073847a6dece06cf7684fc3f5b6a
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-15 19:08:35 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@
 int print_last_digit(int n)
 {
 	int lastDigit;
-	
+
 	if (n >= 0)
 		lastDigit = n % 10;
 	else
 		lastDigit = (n % 10) * -1;
 	_putchar(lastDigit + '0');
-	return lastDigit;
+	return (lastDigit);
 }
