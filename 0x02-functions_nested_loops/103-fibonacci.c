abf996748e3aac167f8e1eee2268c4802b6e82ac
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-15 19:59:29 +0200

    Added

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Computes and prints the sum
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n = 1;
+	long int m = 2;
+	int max;
+
+	printf("%ld, %ld, ", n, m);
+	for (max = 0; max < 48; max++)
+	{
+		if (m + n > 4000000)
+			break;
+		printf("%ld", m + n);
+		m += n;
+		n = m - n;
+		if (max != 47)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
