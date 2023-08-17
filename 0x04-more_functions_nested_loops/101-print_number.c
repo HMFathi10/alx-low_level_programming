c6fcde24c8950c02deb0ee4f5d57277508f29a45
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-17 10:04:14 -0700

    Added

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ *@n: is an integer
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i = 10;
+	int len;
+	int neg = 0;
+
+	while (1)
+	{
+		if (n < 0)
+		{
+			neg = 1;
+			n *= -1;
+		}
+		len = n / i;
+		if (len < 10)
+			break;
+		i *= 10;
+	}
+
+	while (i >= 1)
+	{
+		if (neg == 1)
+		{
+			_putchar(' ');
+			neg = 0;
+		}
+		_putchar((n % i) / (i / 10) + 48);
+		n %= i;
+		i /= 10;
+	}
+}
