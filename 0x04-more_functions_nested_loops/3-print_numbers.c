fa838fcad3c1421d92a4e40ec0506d7a55318eaa
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-17 07:00:19 -0700

    Added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9.
- * 
+ *
  * Return: void
  */
 void print_numbers(void)
 {
-  int i = 0;
+int i = 0;
 
-  do{
-    _putchar(i + 48);
-    i++;
-  } while (i >= 0 && i <= 9);
-  _putchar('\n');
+do {
+_putchar(i + 48);
+i++;
+} while (i >= 0 && i <= 9);
+_putchar('\n');
 }
