9a5e5d3458d3e7b80655a64c33d034d410767b57
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-21 17:26:14 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,24 +2,18 @@
 
 /**
  * print_rev - Call printRec.
- * printRec - Pritn string
  * @s: is a string
  * Return: Void
  */
-void printRec(char *s)
-{
-	if (*(s + 1) != 0)
-		printRec(s + 1);
-	_putchar(*(s));
-}
-
 void print_rev(char *s)
 {
 	if (*s == 0)
 		_putchar('\n');
 	else
 	{
-		printRec(s);
+		if (*(s + 1) != 0)
+			printRec(s + 1);
+		_putchar(*(s));
 		_putchar('\n');
 	}
 }
