b5a37f9fe0abeb5dd4ff08c94b865aa9bbbc070e
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-22 00:18:55 +0200

    Added

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * main - generate password
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int num;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,3 +14,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
