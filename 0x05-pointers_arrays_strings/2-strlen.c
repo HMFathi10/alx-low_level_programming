566af03f6e9e0baec3a35b66a9474533065ceeca
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-21 13:24:32 +0200

    Added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Change value of integer.
+ * @s: is a string
+ * Return: Void
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '0')
+	{
+		i++;
+	}
+	return (i);
+}
