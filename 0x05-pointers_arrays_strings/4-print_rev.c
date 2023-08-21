d78c7c519b3558652da376d8e350958c92daadec
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-21 16:09:20 +0200

    Added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Print string.
+ * @s: is a string
+ * Return: Void
+ */
+void printRec(char *s)
+{
+	if (*(s + 1) != 0)
+		printRec(s + 1);
+	_putchar(*(s));
+}
+
+void print_rev(char *s)
+{
+	printRec(s);
+	_putchar('\n');
+}
