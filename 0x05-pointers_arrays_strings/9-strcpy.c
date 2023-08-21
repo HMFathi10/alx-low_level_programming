4aba807bf390d30389d4f0d47f4d8a8f3aba5556
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-21 22:42:14 +0200

    Added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Reverse.
+ * @dest: is a array
+ * @src: is the size
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (1)
+	{
+		*(dest + i) = *(src + i);
+		if (*(src + i) == 0)
+			break;
+	}
+	return dest;
+}
