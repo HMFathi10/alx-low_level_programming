55c85cc78d36667d7ee81f2a29f28f780f3551d4
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-21 20:52:45 +0200

    Added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Call printRec.
+ * @s: is a string
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char *a;
+
+	while (s[i] != 0)
+		i++;
+	char a[i - 1];
+
+	while (i--)
+	{
+		a[j++] = s[i];
+	}
+	while (i < j)
+		s[++i] = a[i];
+}
