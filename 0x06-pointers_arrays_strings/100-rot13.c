ab01e3bf96986bb973a02487977acc37af49cc85
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-24 08:58:16 -0700

    Added

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *rot13 - Change string to uppercase
+ * @s: is a string
+ * Return: char
+ */
+char *rot13(char *s)
+{
+	char *sym = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int *eq = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int st = -1;
+	int j = 0;
+
+	while (s[++st] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[st] == sym[j])
+			{
+				s[st] = eq[j];
+				break;
+			}
+			else if (s[st] == eq[j])
+			{
+				s[st] = sym[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
