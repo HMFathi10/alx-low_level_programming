74515d319a3265a9c7bc199bf4687e824ca66c6c
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-24 08:06:55 -0700

    Added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *leet - Change string to uppercase
+ * @s: is a string
+ * Return: char
+ */
+char *leet(char *s)
+{
+	char *sym = "aAeEoOtTlL";
+	int eq[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	int st = -1;
+	int j = 0;
+
+	while (s[++st] != '\0')
+	{
+		for (j = 0; j < 9; j++)
+		{
+			if (s[st] == sym[j])
+			{
+				s[st] = eq[j] + '0';
+			}
+		}
+	}
+	return (s);
+}
