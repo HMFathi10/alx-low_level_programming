df5fc2779061fe355d658c8c6e60db1559ed6197
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-28 18:03:47 +0200

    Added

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *print_diagsums - sum.
+ * @a: Square D array
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += a[i];
+		a += size;
+	}
+	printf("%d, ", sum);
+	sum = 0;
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum += a[i];
+		a -= size;
+	}
+	printf("%d\n", sum);
+}
