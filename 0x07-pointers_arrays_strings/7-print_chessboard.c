33da7c8e4f75d5622acf9263d009c91ae0006ea6
Hossam Mahmoud <hossamfci10@gmail.com>
2023-08-28 17:51:02 +0200

    Added

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,9 +12,8 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i <= 7; i++)
 	{
-		if (i == 0 || i == 1 || i == 6 || i == 7)
-			for (j = 0; j <= 7; j++)
-				printf("%c", a[i][j]);
+		for (j = 0; j <= 7; j++)
+			printf("%c", a[i][j]);
 		printf("\n");
 	}
 }
