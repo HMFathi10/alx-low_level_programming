e75b6080ac746693d8a1bdc7c52cb33b45777c85
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-08-30 19:12:02 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - Return length.
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (_strlen_recursion(s + 1) + 1);
+	return (0);
+}
+
+/**
  * check - Return the power.
  * @i: number
  * @size: number
