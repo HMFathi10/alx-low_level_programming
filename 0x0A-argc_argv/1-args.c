0c51254f85bda4063cbfefb50c4c96caf2c293c7
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-01 10:31:07 -0700

    Added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point of program
+ *
+ * Description: Printing sentence using the puts function
+ * @argc: integer
+ * @argv: array of string
+ * Return: value is 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
