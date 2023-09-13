0990dea975ca3d6942bcef6775fc5f9bedd92b12
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-13 22:14:19 +0200

    Added

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Print name.
+ * @array: int
+ * @size: int
+ * @cmp: pointer to function
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+	return (-1);
+
+}
