c4e49282898acb695b21350185bfe12ea2124782
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-09-14 13:36:10 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,31 +9,34 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
+	int flag = 0, j;
+	char symbol[] = "cifs";
 	char *str;
 
 	va_start(ap, format);
 	while (format && format[i])
 	{
+		j = 0;
+		while (symbol[j])
+		{
+			if (format[i] == symbol[j] && flag++)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
 		switch (format[i])
 		{
 			case 'c':
-				if (i != 0)
-					printf(", ");
 				printf("%c", va_arg(ap, int));
 				break;
 			case 'i':
-				if (i != 0)
-					printf(", ");
 				printf("%d", va_arg(ap, int));
 				break;
 			case 'f':
-				if (i != 0)
-					printf(", ");
 				printf("%f", va_arg(ap, double));
 				break;
 			case 's':
-				if (i != 0)
-					printf(", ");
 				str = va_arg(ap, char *);
 				!str ? printf("(nil)") : printf("%s", str);
 				break;
