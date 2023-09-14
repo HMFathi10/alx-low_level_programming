ce2b9ab43cc5d5a10f9bd731beefe4028f12d24c
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-14 06:19:30 +0200

    Added

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - SUM.
+ * @n: int
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int sum = 0, i = 0;
+
+	va_start(ap, n);
+	if (n)
+		for(; i < n; i++)
+			sum += va_arg(ap, int);
+	va_end (ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/*a list that was given from the task 0 */
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
