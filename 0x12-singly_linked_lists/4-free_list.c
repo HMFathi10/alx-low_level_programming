38d685b071aa11ff204feb916404aadfdc204045
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-21 07:44:42 +0200

    Added

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Free the linked list.
+ * @head: list_h
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while(head)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+		temp = head;
+	}
+}
