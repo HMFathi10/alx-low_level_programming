2ea9e258200ca3ad99697280fa2bdd3281d9bd40
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-21 07:34:43 +0200

    Added

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * str_len - get string length
+ * @str: char
+ * Return: int
+ */
+int str_len(const char *str)
+{
+	int len = -1;
+
+	while (str[++len])
+		;
+	return (len);
+}
+
+/**
+ * *add_node_end - Return new node.
+ * @head: list_h
+ * @str: char
+ * Return: list_t
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = str_len(str);
+	new->next = NULL;
+	temp = *head;
+	if (!temp)
+		*head = new;
+	else
+	{
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (*head);
+}
