fed5a96e33a8c98ed7e854ac12dad701edfeec58
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-25 07:28:39 +0200

    Added

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - print the list and return length.
+ * @head: listint_t
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+
+	temp = head;
+	while(temp)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}	
+}
