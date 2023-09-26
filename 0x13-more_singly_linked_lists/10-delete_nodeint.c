fb1a73355ed6c0cb5cd782e04a3c3a8eed8c9af7
Hossam Mahmoud <hossamfci10@gmail.com>
2023-09-26 07:54:27 +0200

    Added

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - print the list and return length.
+ * @head: listint_t
+ * @index: int
+ * Return: int
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *ptr;
+	unsigned int i;
+
+	temp = *head;
+	if (index == 0)
+	{
+		ptr = temp->next;
+		*head = ptr;
+		free(temp);
+		return (1);
+	}
+	for (i = 0; i < index && temp; i++)
+	{
+		if (i == index - 1)
+		{
+			ptr = temp->next;
+			temp->next = ptr->next;
+			free(ptr);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	return (-1);
+}
