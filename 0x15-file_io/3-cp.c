c3c31bbc56ef86edfcdc3425c041eb8c816c754f
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-10-02 12:52:41 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * catch_error - catch the error.
+ * @case1: int.
+ * @case2: int.
+ * @argv: arguments vector.
+ * Return: void.
+ */
+void catch_error(int case1, int case2, char *argv[])
+{
+	if (case1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (case2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
  * main - check the code.
  * @argc: number of arguments.
  * @argv: arguments vector.
@@ -19,31 +40,19 @@ int main(int argc, char *argv[])
 	}
 	fdfrom = open(argv[1], O_RDONLY);
 	if (fdfrom == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		catch_error(fdfrom, 0, argv);
 	fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fdto == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		catch_error(0, fdto, argv);
 	numBytesRead = 1024;
 	while (numBytesRead == 1024)
 	{
 		numBytesRead = read(fdfrom, buffer, 1024);
 		if (numBytesRead == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			catch_error(-1, 0, argv);
 		numBytesWrite = write(fdto, buffer, numBytesRead);
 		if (numBytesWrite == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			catch_error(0, -1, argv);
 	}
 	if (close(fdfrom) == -1)
 	{
