2138c0461085aff8de161fcea4746ef793c222b8
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-10-02 12:24:08 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,37 +13,48 @@ int main(int argc, char *argv[])
 	int fdfrom, fdto;
 	ssize_t numBytesRead, numBytesWrite;
 
-	if (argc == 3)
-		printf("argc 3");
-	/*if (argc != 3)
+	if (argc != 3)
 	{
-		printf("1");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
-	}*/
+	}
 	fdfrom = open(argv[1], O_RDONLY);
-	/*if (fdfrom == -1)
+	if (fdfrom == -1)
 	{
-		printf("2");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}*/
+	}
 	fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	/*if (fdto == -1)
+	if (fdto == -1)
 	{
-		printf("3");
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-	}*/
+	}
 	numBytesRead = 1024;
 	while (numBytesRead == 1024)
 	{
 		numBytesRead = read(fdfrom, buffer, 1024);
+		if (numBytesRead == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		numBytesWrite = write(fdto, buffer, numBytesRead);
-		/*if (numBytesRead == -1 || numBytesWrite == -1)
-			exit (98);*/
-		if (numBytesWrite != -1)
-			printf("Not equal -1");
+		if (numBytesWrite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-	close(fdfrom);
-	close(fdto);
-
+	if (close(fdfrom) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(fdto) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
