6aa7da5e5e935a44ea129a111bbc2aa2d21b4924
Hossam Mahmoud <hossamfci10@gmail.com>
2023-11-09 09:31:35 -0800

    Added

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * *sum_dnodeint - Get node from  double linked list .
+ * @head: dlistint_t
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int count = 0;
+
+	if (!head)
+		return (count);
+	while (head->prev)
+		head = head->prev;
+	while (head && count++)
+	{
+		head = head->next;
+	}
+	return (count);
+}
