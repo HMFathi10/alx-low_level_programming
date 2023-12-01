232c581f2fc275ee7f6ea2310ecab7f71f95140e
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-12-01 13:01:05 +0200

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,17 +9,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table;
+	hash_node_t **array;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t *));
-	if (table->array == NULL)
+	array = malloc(size * sizeof(hash_node_t *));
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+	table->array = array;
 
 	return (table);
 }
