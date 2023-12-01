d446edeb23f012b83bbd3cb80536c3847a52de0e
Hossam Mahmoud <hossamfci10@gmail.com>
2023-12-01 04:13:33 -0800

    Added

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,7 +17,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		temp = ht->array[i];
-		while((col = temp) != NULL)
+		while ((col = temp) != NULL)
 		{
 			temp = temp->next;
 			free(col->key);
