a310ee07ba54d4f526eaf5bcf07c73f4721dc01a
HMFathi10 <hossamfci10@gmail.com>
2023-12-01 13:18:40 +0200

    Added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,8 +8,7 @@
  *
  * Return: 1 on success, 0 on failure
  */
-int
-hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *node;
@@ -34,4 +33,5 @@ hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 	node->next = ht->array[index];
 	ht->array[index] = node;
+	return (1);
 }
