be25c7f106de8c1ca03a2341d874207f787d7e53
HMFathi10 <hossamfci10@gmail.com>
2023-12-01 14:08:39 +0200

    Added

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: pointer to the hash table
+ * Return: no return
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t temp;
+	hash_node_t col;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while((col = temp) != NULL)
+		{
+			temp = temp->next;
+			free(col->key);
+			free(col->value);
+			free(col);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
